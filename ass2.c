1a21394ec8fb23f733cdfeb499a965fa9ea40063
Ayushinitp <116718528+Ayushinitp@users.noreply.github.com>
2022-10-27 10:02:30 +0530

    Add files via upload

---
+++ ass2.c
@@ -0,0 +1,121 @@
+//question 1
+/*
+#include <stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter three numbers : ");
+    scanf("%d%d%d",&a,&b,&c);
+    float avg=(a+b+c)/3.0;
+    printf("Average is %f",avg);
+    return 0;
+}
+*/
+
+//question 2
+/*
+#include <stdio.h>
+int main(){
+    float r,cir;
+    printf("Enter the radius of circle : ");
+    scanf("%f",&r);
+    cir=2*3.14*r;
+    printf("Circumference of a circle is : %f",cir);
+    return 0;
+    }
+    */
+
+   //question 3
+   /*
+   #include <stdio.h>
+   int main(){
+   float p,r,t , si;
+    printf("Enter principal , rate , time respectively : ");
+    scanf("%f%f%f",&p,&r,&t);
+    si=(p*r*t)/100;
+    printf("Simple interest is : %.3f",si);
+    return 0;
+   }
+   */
+
+  //question 4
+  /*
+  #include <stdio.h>
+  int main(){
+    float a , b, c, vol;
+    printf("Enter length , breadth , height respectively : ");
+    scanf("%f%f%f",&a,&b,&c);
+    vol=a*b*c;
+    printf("Volume of cuboid is : %f",vol);
+    return 0;
+  }
+  */
+
+
+//question 5
+
+
+
+
+
+
+ //question 6
+/*
+ #include <stdio.h>
+ int main(){
+    char a;
+    printf("Enter a character : ");
+    scanf("%c",&a);
+    printf("ASCII code = %d",a);
+    return 0;
+ }
+ */
+
+
+//question 7
+/*
+#include <stdio.h>
+int main(){
+    int x;
+    printf("Enter ASCII code : ");
+    scanf("%d",&x);
+    printf("Corresponding character = %c",x);
+    return 0;
+}
+*/
+
+//question 8
+/*
+#include<stdio.h>
+int main(){
+    char a1,b1,c1;
+    printf("Enter three characters : ");
+    scanf("%c%c%c",&a1,&b1,&c1);
+    printf("Corresponding ASCII Code = %d , %d , %d", a1,b1,c1);
+    return 0;
+    }
+*/
+
+//question 9
+/*
+#include <stdio.h>
+int main(){
+    int DD,MM,YYYY;
+    printf("Enter day , month , year respectively : ");
+    scanf("%d%d%d",&DD,&MM,&YYYY);
+    printf("Day - %d , Month - %d , Year - %d",DD,MM,YYYY);
+    return 0;
+
+}
+*/
+
+//question 10
+/*
+#include <stdio.h>
+int main(){
+    int HH , MM ;
+    printf("Enter hour and minute respectively : ");
+    scanf("%d%d",&HH,&MM);
+    printf("%d hour and %d minute ",HH,MM);
+    return 0;
+}
+*/
